454d3e0a488e9bb84283f18b979e816f1679b17e
NJBlore <njblore@outlook.com>
2019-04-01 13:05:19 +0100

    create article table

--- db/migrations/20190401120324_create_news.js
+++ db/migrations/20190401120324_create_news.js
@@ -2,15 +2,40 @@ exports.up = function(connection, Promise) {
   console.log('creating users table...');
   return connection.schema
     .createTable('users', usersTable => {
-      usersTable.string('username').primary();
+      usersTable
+        .string('username')
+        .primary()
+        .unique();
       usersTable.string('name');
       usersTable.string('avatar_url');
     })
     .then(() => {
       return connection.schema.createTable('topics', topicsTable => {
         topicsTable.string('description');
-        topicsTable.string('slug').notNullable();
-        topicsTable.increments('topic_id');
+        topicsTable
+          .string('slug')
+          .notNullable()
+          .unique();
+        topicsTable.increments('topic_id').primary();
+      });
+    })
+    .then(() => {
+      return connection.schema.createTable('articles', articlesTable => {
+        articlesTable.increments('article_id').primary();
+        articlesTable.string('title');
+        articlesTable.string('topic');
+        articlesTable
+          .foreign('topic')
+          .references('slug')
+          .inTable('topics');
+        articlesTable.string('author');
+        articlesTable
+          .foreign('author')
+          .references('username')
+          .inTable('users');
+        articlesTable.string('body');
+        articlesTable.integer('votes');
+        articlesTable.timestamp('created_at');
       });
     });
 };
