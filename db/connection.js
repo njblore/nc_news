61d2910fbf56eebe8adadf85f43163b92ca03eda
njblore <njblore@outlook.com>
2019-04-04 10:12:09 +0100

    add heroku info

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,4 @@
-const dbConfig = require('../knexfile');
+const { dbConfig } =
+  process.env.NODE_ENV === 'production' ? process.env : require('../knexfile');
 
 module.exports = require('knex')(dbConfig);
